8da23674afdb6bd82a1e85cf8224a1776c35c460
Jarrod Millman <jarrod.millman@gmail.com>
2014-07-26 23:18:11 -0700

    RF: structure for expansion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='score',
-    version='0.1',
-    py_modules=['score'],
-    entry_points=dict(console_scripts=['gb-score = score:main']),
+    version='0.11',
+    packages=['gradebook'],
+    entry_points=dict(console_scripts=['gb-score = gradebook.score:main']),
 )
